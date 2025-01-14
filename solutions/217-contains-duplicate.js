d3fdcea5cd25235c61f6458443a2baf06a6e3bf4
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2025-01-13 21:25:36 -0600

    Refactor solution for problem 217: Contains Duplicate to improve performance and fix Time Limit Exceeded error caused by nested loops

--- solutions/217-contains-duplicate.js
+++ solutions/217-contains-duplicate.js
@@ -1,12 +1,27 @@
-let containsDuplicate = function(nums) {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] === nums[j]) {
-        return true;
-      }
+// Solution 1:
+// let containsDuplicate = function(nums) {
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[i] === nums[j]) {
+//         return true;
+//       }
+//     }
+//   }
+//   return false;
+// }
+
+// Solution 2:
+const containsDuplicate = (nums) => {
+  let uniqueNums = new Set(); // Create an empty Hash Set
+
+  for (let num of nums) { // Use a for...of loop to iterate over the nums array
+    if (uniqueNums.has(num)) { // Check if the current number already exists in the Hash Set
+      return true; // If the number exists, return true (nums contains duplicates)
     }
+    uniqueNums.add(num); // Otherwise, add the current number to the Hash Set
   }
-  return false;
+
+  return false; // If the loop finishes, return false (nums doesn't contain duplicates)
 }
 
 let case1 = [1, 2, 3, 1];
