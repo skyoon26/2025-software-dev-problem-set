5a99386fc35d9b5d9ef72245de75d3875fdfb532
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2025-02-13 14:23:51 -0600

    Refactor solution for Leetcode problem 242: Valid Anagram by simplifying if statement

--- solutions/242-valid-anagram.js
+++ solutions/242-valid-anagram.js
@@ -1,12 +1,24 @@
-let isAnagram = function(s, t) {
-  let str1 = s.split('').sort().join('');
-  let str2 = t.split('').sort().join('');
+// Solution 1:
+// let isAnagram = function(s, t) {
+//   let str1 = s.split('').sort().join('');
+//   let str2 = t.split('').sort().join('');
 
-  if (str1 === str2) {
-    return true;
+//   if (str1 === str2) {
+//     return true;
+//   }
+
+//   return false;
+// }
+
+// Solution 2:
+const isAnagram = (s, t) => {
+
+  if (s.length !== t.length) {
+    return false;
+  } else {
+    return s.split('').sort().join('') === t.split('').sort().join('');
   }
-
-  return false;
+  
 }
 
 let s = "anagram";
